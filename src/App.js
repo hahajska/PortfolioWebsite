7f432b3fecc7058859062c9c74ed83ed1f13efb3
TomasSchlogl <thomas537@seznam.cz>
2021-04-24 18:43:02 +0200

    finalsetup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.scss";
-import Main from "./components/Main";
-import Navbar from "./components/Navbar";
-//import ProgressiveImage from "react-progressive-image";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Main />
+      <h1>hello</h1>
     </>
   );
 }
--- src/components/Main/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Main() {
-  return <section className="h-screen bg-white"></section>;
-}
