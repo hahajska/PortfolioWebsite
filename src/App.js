3a0035f77ebe880eb9df777e8d83ff2fb3847330
TomasSchlogl <thomas537@seznam.cz>
2021-04-29 18:07:55 +0200

    front page bez breakpoints done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import "./App.scss";
-import Mountains from "./images/moutains.jpg";
-import { CSSRulePlugin } from "gsap/CSSRulePlugin";
-import { TimelineLite, Power2, gsap } from "gsap";
+import Main from "./Components/Main";
 
 function App() {
-  let image = useRef(null);
-  let container = useRef(null);
-  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
-
-  let tl = new TimelineLite();
-
-  useEffect(() => {
-    tl.to(container, 0, { css: { visibility: "visible" } });
-    tl.to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut });
-    tl.from(image, 1.4, {
-      scale: 1.6,
-      ease: Power2.easeInOut,
-      delay: -1.4,
-    });
-  });
-
   return (
-    <section className="main">
-      <div ref={(el) => (container = el)} className="container">
-        <>
-          <div className="img-container">
-            <img ref={(el) => (image = el)} src={Mountains} alt="" />
-          </div>
-        </>
-      </div>
-      <div className="space"></div>
-    </section>
+    <>
+      <Main />
+    </>
   );
 }
 
