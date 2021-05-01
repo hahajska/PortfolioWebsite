ce1b98433ba8b311ca69c7729ae3fbc4631ec67c
TomasSchlogl <thomas537@seznam.cz>
2021-05-01 12:38:28 +0200

    fix bug 2nd

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,13 @@
-// craco.config.js
 module.exports = {
-  purge: [
-    "./pages/**/*.vue",
-    "./components/**/*.vue",
-    "./plugins/**/*.vue",
-    "./static/**/*.vue",
-    "./store/**/*.vue",
-  ],
-  theme: {},
-  variants: {},
-  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "components/**/*.vue",
+      "layouts/**/*.vue",
+      "pages/**/*.vue",
+      "plugins/**/*.js",
+      "nuxt.config.js",
+    ],
+  },
 };
